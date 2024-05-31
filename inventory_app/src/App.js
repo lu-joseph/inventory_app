ba4d96d2a5648e2dd466a845d7705eb6a21ce23d
Joseph Lu <josephlu414@gmail.com>
2024-05-30 21:44:33 -0700

    restructure

--- inventory_app/src/App.js
+++ inventory_app/src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{!data ? "Loading..." : data}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
