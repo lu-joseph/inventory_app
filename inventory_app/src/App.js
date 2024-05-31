eace007f27299246c4552cd4018b95d6cb14a5ed
Joseph Lu <josephlu414@gmail.com>
2024-05-31 09:21:30 -0700

    basic features

--- inventory_app/src/App.js
+++ inventory_app/src/App.js
@@ -1,30 +1,85 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Button from "react-bootstrap/Button";
 import { useState, useEffect } from "react";
 
-function App() {
-  const [data, setData] = useState(null);
+const defaultHeader = { "Content-Type": "application/json" };
 
+function ProductCard(props) {
+  const [cost, setCost] = useState(0);
+  const [numSelected, setNumSelected] = useState(0);
   useEffect(() => {
-    fetch("/api")
+    fetch("/cost/" + props.name, {
+      method: "GET",
+      headers: defaultHeader,
+    })
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setCost(data.price));
   }, []);
+  return (
+    <div>
+      <h3>Cost: ${cost}</h3>
+      <Button
+        onClick={() => {
+          setNumSelected(0);
+          fetch("/purchase/" + props.name + "/" + numSelected, {
+            method: "PUT",
+            headers: defaultHeader,
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+            });
+        }}
+      >
+        {props.name.replaceAll("_", " ")} {props.type}
+      </Button>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Button
+              onClick={() => setNumSelected(Math.max(numSelected - 1, 0))}
+            >
+              -
+            </Button>
+            {numSelected}
+            <Button onClick={() => setNumSelected(numSelected + 1)}>+</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
+function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <h1>Totes</h1>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <ProductCard name="Rockstar_tour" type="tote"></ProductCard>
+          </div>
+          <div className="col">
+            <ProductCard name="Star_girls" type="tote"></ProductCard>
+          </div>
+          <div className="col">
+            <ProductCard name="Digital_nostalgia" type="tote"></ProductCard>
+          </div>
+          <div className="col">
+            <ProductCard name="Whats_on_your_mind" type="tote"></ProductCard>
+          </div>
+          <div className="col">
+            <ProductCard name="Roommates" type="tote"></ProductCard>
+          </div>
+          <div className="col">
+            <ProductCard name="Mona_plus_pals" type="tote"></ProductCard>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- server/server/index.js
+++ server/server/index.js
@@ -1,12 +1,86 @@
 const path = require("path");
+require("dotenv").config();
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const { Client } = require("@notionhq/client");
+const notion = new Client({
+  auth: process.env.NOTION_KEY,
+});
+
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.static(path.resolve(__dirname, "../inventory_app/build")));
+
+async function getPageByProductName(productName) {
+  return await notion.databases.query({
+    database_id: process.env.NOTION_PRODUCT_DB_ID,
+    filter: {
+      property: "Product name",
+      rich_text: {
+        equals: productName,
+      },
+    },
+  });
+}
+
+app.get("/cost/:item_name", async (req, resp) => {
+  const itemName = req.params.item_name.replaceAll("_", " ");
+  const searchResults = await getPageByProductName(itemName);
+  const result = searchResults.results ? searchResults.results[0] : undefined;
+  console.log(result);
+  const cost = result ? result.properties["Price"].number : 0;
+  resp.send({ price: cost });
+});
+
+app.put("/purchase/:item_name/:num_sold", async (req, resp) => {
+  const numSold = parseInt(req.params.num_sold);
+  const itemName = req.params.item_name.replaceAll("_", " ");
+  const searchResults = await getPageByProductName(itemName);
+  const result = searchResults.results ? searchResults.results[0] : undefined;
+  if (result) {
+    const response = await notion.pages.update({
+      page_id: result.id,
+      properties: {
+        "Total sold": {
+          type: "number",
+          number: result.properties["Total sold"].number + numSold,
+        },
+      },
+    });
+  }
+  const createResponse = await notion.pages.create({
+    parent: {
+      type: "database_id",
+      database_id: process.env.NOTION_SALES_DB_ID,
+    },
+    properties: {
+      "Product name": {
+        title: [
+          {
+            text: {
+              content: itemName,
+            },
+          },
+        ],
+      },
+      "Cost per item": {
+        type: "number",
+        number: result.properties["Price"].number,
+      },
+      "Number items": {
+        type: "number",
+        number: numSold,
+      },
+    },
+  });
+});
+
+app.get("/", (req, resp) => {
+  res.sendFile(path.resolve(__dirname, "../inventory_app/build", "index.html"));
+});
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
@@ -14,7 +88,7 @@ app.get("/api", (req, res) => {
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../inventory_app/build", "index.html"));
 });
 
 app.listen(PORT, () => {
