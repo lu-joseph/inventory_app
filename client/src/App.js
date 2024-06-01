50fceb0acc7039a7ebebf10911a44ee161295cc6
Joseph Lu <josephlu414@gmail.com>
2024-05-31 23:47:15 -0700

    productSale, Sales tables connected

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,10 @@ import { useState, useEffect } from "react";
 
 const defaultHeader = { "Content-Type": "application/json" };
 
+// Card: {name: string, numItems: number, costPerItem: number}
+
 function ProductCard(props) {
+  // props: {name: string, cards: [Card], setCards: () => void}
   const [cost, setCost] = useState(0);
   const [numSelected, setNumSelected] = useState(0);
   useEffect(() => {
@@ -13,36 +16,82 @@ function ProductCard(props) {
       headers: defaultHeader,
     })
       .then((res) => res.json())
-      .then((data) => setCost(data.price));
+      .then((data) => {
+        setCost(data.price);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (props.cards.length === 0) {
+      console.log("resetting");
+      setNumSelected(0);
+      fetch("/cost/" + props.name, {
+        method: "GET",
+        headers: defaultHeader,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setCost(data.price);
+        });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.cards]);
+
+  const setCards = props.setCards;
+
+  useEffect(() => {
+    setCards(
+      props.cards
+        .filter((val) => val.name !== props.name)
+        .concat([
+          { name: props.name, numItems: numSelected, costPerItem: cost },
+        ])
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cost, numSelected]);
+
   return (
-    <div>
-      <h3>Cost: ${cost}</h3>
-      <Button
-        onClick={() => {
-          setNumSelected(0);
-          fetch("/purchase/" + props.name + "/" + numSelected, {
-            method: "PUT",
-            headers: defaultHeader,
-          })
-            .then((res) => res.json())
-            .then((data) => {
-              console.log(data);
-            });
-        }}
-      >
-        {props.name.replaceAll("_", " ")} {props.type}
-      </Button>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <Button
-              onClick={() => setNumSelected(Math.max(numSelected - 1, 0))}
-            >
-              -
-            </Button>
-            {numSelected}
-            <Button onClick={() => setNumSelected(numSelected + 1)}>+</Button>
+    <div className="col-sm">
+      <div className="product-card">
+        {/* <h3>Cost: ${cost}</h3> */}
+        <div className="input-group mb-3">
+          <span className="input-group-text">$</span>
+          <input
+            type="text"
+            className="form-control"
+            aria-label="Dollar amount (with dot and two decimal places)"
+            value={cost}
+            onChange={(e) => {
+              setCost(
+                e.target.value
+                  .split("")
+                  .filter((char) => !isNaN(parseInt(char)))
+                  .join("")
+              );
+            }}
+          />
+        </div>
+        <h3>
+          {props.name.replaceAll("_", " ")} {props.type}
+        </h3>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Button
+                className="quantity-change"
+                onClick={() => setNumSelected(Math.max(numSelected - 1, 0))}
+              >
+                -
+              </Button>
+              {numSelected}
+              <Button
+                className="quantity-change"
+                onClick={() => setNumSelected(numSelected + 1)}
+              >
+                +
+              </Button>
+            </div>
           </div>
         </div>
       </div>
@@ -51,6 +100,7 @@ function ProductCard(props) {
 }
 
 function App() {
+  const [selectedItems, setSelectedItems] = useState([]);
   return (
     <div className="App">
       <div className="container">
@@ -60,23 +110,61 @@ function App() {
       </div>
       <div className="container">
         <div className="row">
+          <ProductCard
+            name="Rockstar_tour"
+            type="tote"
+            cards={selectedItems}
+            setCards={setSelectedItems}
+          ></ProductCard>
+          <ProductCard
+            name="Star_girls"
+            type="tote"
+            cards={selectedItems}
+            setCards={setSelectedItems}
+          ></ProductCard>
+          <ProductCard
+            name="Digital_nostalgia"
+            type="tote"
+            cards={selectedItems}
+            setCards={setSelectedItems}
+          ></ProductCard>
+          <ProductCard
+            name="Angel_bathtub"
+            type="tote"
+            cards={selectedItems}
+            setCards={setSelectedItems}
+          ></ProductCard>
+          <ProductCard
+            name="Roommates"
+            type="tote"
+            cards={selectedItems}
+            setCards={setSelectedItems}
+          ></ProductCard>
+          <ProductCard
+            name="Mona_pals"
+            type="tote"
+            cards={selectedItems}
+            setCards={setSelectedItems}
+          ></ProductCard>
+        </div>
+        <div className="row">
           <div className="col">
-            <ProductCard name="Rockstar_tour" type="tote"></ProductCard>
-          </div>
-          <div className="col">
-            <ProductCard name="Star_girls" type="tote"></ProductCard>
-          </div>
-          <div className="col">
-            <ProductCard name="Digital_nostalgia" type="tote"></ProductCard>
-          </div>
-          <div className="col">
-            <ProductCard name="Whats_on_your_mind" type="tote"></ProductCard>
-          </div>
-          <div className="col">
-            <ProductCard name="Roommates" type="tote"></ProductCard>
-          </div>
-          <div className="col">
-            <ProductCard name="Mona_plus_pals" type="tote"></ProductCard>
+            <Button
+              onClick={() => {
+                console.log(selectedItems);
+                fetch("/purchase", {
+                  method: "PUT",
+                  headers: defaultHeader,
+                  body: JSON.stringify({ items: selectedItems }),
+                }).then((resp) => {
+                  console.log("response:");
+                  console.log(resp);
+                  setSelectedItems([]);
+                });
+              }}
+            >
+              Submit
+            </Button>
           </div>
         </div>
       </div>
--- server/server/index.js
+++ server/server/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
 
 const { Client } = require("@notionhq/client");
 const notion = new Client({
@@ -14,13 +15,14 @@ const notion = new Client({
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../../client/build")));
 
+// productName has underscore
 async function getPageByProductName(productName) {
   return await notion.databases.query({
     database_id: process.env.NOTION_PRODUCT_DB_ID,
     filter: {
       property: "Product name",
       rich_text: {
-        equals: productName,
+        equals: productName.replaceAll("_", " "),
       },
     },
   });
@@ -30,52 +32,93 @@ app.get("/cost/:item_name", async (req, resp) => {
   const itemName = req.params.item_name.replaceAll("_", " ");
   const searchResults = await getPageByProductName(itemName);
   const result = searchResults.results ? searchResults.results[0] : undefined;
-  console.log(result);
+  // console.log(result);
   const cost = result ? result.properties["Price"].number : 0;
   resp.send({ price: cost });
 });
 
-app.put("/purchase/:item_name/:num_sold", async (req, resp) => {
-  const numSold = parseInt(req.params.num_sold);
-  const itemName = req.params.item_name.replaceAll("_", " ");
-  const searchResults = await getPageByProductName(itemName);
-  const result = searchResults.results ? searchResults.results[0] : undefined;
-  if (result) {
-    const response = await notion.pages.update({
-      page_id: result.id,
-      properties: {
-        "Total sold": {
-          type: "number",
-          number: result.properties["Total sold"].number + numSold,
-        },
-      },
-    });
-  }
-  const createResponse = await notion.pages.create({
+// body: {items: [Card]}
+// Card: {name: string, numItems: number, costPerItem: number}
+app.put("/purchase", async (req, resp) => {
+  const items = req.body.items;
+
+  const salesCreatePageResponse = await notion.pages.create({
     parent: {
       type: "database_id",
       database_id: process.env.NOTION_SALES_DB_ID,
     },
     properties: {
-      "Product name": {
-        title: [
-          {
-            text: {
-              content: itemName,
-            },
-          },
-        ],
-      },
-      "Cost per item": {
+      "Total number items": {
         type: "number",
-        number: result.properties["Price"].number,
+        number: items.reduce((acc, cur) => acc + cur.numItems, 0),
       },
-      "Number items": {
+      "Total cost": {
         type: "number",
-        number: numSold,
+        number: items.reduce(
+          (acc, cur) => acc + cur.numItems * cur.costPerItem,
+          0
+        ),
       },
     },
   });
+  const saleId = salesCreatePageResponse.properties["ID"].unique_id.number;
+  items.forEach(async (product) => {
+    const numSold = parseInt(product.numItems);
+    if (numSold > 0) {
+      const itemName = product.name;
+      const costPerItem = parseInt(product.costPerItem);
+      const searchResults = await getPageByProductName(itemName);
+      const result = searchResults.results
+        ? searchResults.results[0]
+        : undefined;
+      const pageId = result.id;
+      const updateResponse = await notion.pages.update({
+        page_id: pageId,
+        properties: {
+          "Total sold": {
+            type: "number",
+            number: result.properties["Total sold"].number + numSold,
+          },
+        },
+      });
+      console.log(`${itemName}: update response: ${updateResponse}`);
+      const createResponse = await notion.pages.create({
+        parent: {
+          type: "database_id",
+          database_id: process.env.NOTION_PRODUCT_SALES_DB_ID,
+        },
+        properties: {
+          "Product name": {
+            title: [
+              {
+                text: {
+                  content: itemName,
+                },
+              },
+            ],
+          },
+          "Cost per item": {
+            type: "number",
+            number: costPerItem,
+          },
+          "Number items": {
+            type: "number",
+            number: numSold,
+          },
+          SaleID: {
+            type: "number",
+            number: saleId,
+          },
+          ProductID: {
+            type: "number",
+            number: result.properties["ID"].unique_id.number,
+          },
+        },
+      });
+      console.log(`${itemName}: create response: ${createResponse}`);
+    }
+  });
+  resp.send({ data: "response" });
 });
 
 app.get("/", (req, res) => {
